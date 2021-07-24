de27639007a75bb92240d40d82a2c751e1dc4765
Semargl92 <gavriljuk.9292@gmail.com>
2021-07-24 03:26:36 +0300

    solution: added hibernate class 'User', transformed field 'gender' to enum

--- src/main/java/by/semargl/SpringBootStarter.java
+++ src/main/java/by/semargl/SpringBootStarter.java
@@ -2,12 +2,14 @@ package by.semargl;
 
 
 import by.semargl.beans.ApplicationBeans;
+import by.semargl.beans.PersistenceBeanConfiguration;
 import by.semargl.beans.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,7 +18,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc
 @EnableCaching
 @EnableSwagger2
+@EnableTransactionManagement
 @Import({ApplicationBeans.class,
+        PersistenceBeanConfiguration.class,
         SwaggerConfig.class})
 public class SpringBootStarter {
 
---
+++ src/main/java/by/semargl/beans/PersistenceBeanConfiguration.java
@@ -0,0 +1,54 @@
+package by.semargl.beans;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+public class PersistenceBeanConfiguration {
+
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        factoryBean.setPackagesToScan("by.semargl");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(getAdditionalProperties());
+        factoryBean.afterPropertiesSet();
+
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
+    }
+
+    @Primary
+    @Autowired
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("by.semargl");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(getAdditionalProperties());
+
+        return em;
+    }
+
+    private Properties getAdditionalProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+        return properties;
+    }
+}
